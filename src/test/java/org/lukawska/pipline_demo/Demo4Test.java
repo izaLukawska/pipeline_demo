5c77e65fb0bed11c9a86dd97dd051b5b5e88ad7d
iza.lukawska <lukawska.cf@gmail.com>
2025-08-26 16:00:11 +0200

    test demo

---
+++ src/test/java/org/lukawska/pipline_demo/Demo4Test.java
@@ -0,0 +1,14 @@
+package org.lukawska.pipline_demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Demo4Test {
+
+    @Test
+    void sayHello() {
+        Demo4 demo4 = new Demo4();
+        assertEquals("Hello World!", demo4.sayHello());
+    }
+}
