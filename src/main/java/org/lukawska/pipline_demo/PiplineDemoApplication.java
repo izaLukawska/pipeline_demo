6214dd6fdb2ca39573b375da8c66b72d74ada1d2
iza.lukawska <lukawska.cf@gmail.com>
2025-08-26 15:20:37 +0200

    init commit

---
+++ src/main/java/org/lukawska/pipline_demo/PiplineDemoApplication.java
@@ -0,0 +1,13 @@
+package org.lukawska.pipline_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiplineDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiplineDemoApplication.class, args);
+    }
+
+}
